b4c817ef7270ee3a7172c3a1d1e1122e3d7aaca1
Anton Matveev <aush.tx@gmail.com>
2016-02-05 05:48:32 +0700

    converted spaces to tabs

--- webpack.config.js
+++ webpack.config.js
@@ -2,26 +2,26 @@ var webpack = require('webpack');
 var path = require('path');
 
 module.exports = {
-  entry: path.join(__dirname, 'index'),
-  output: {
-    path: path.join(__dirname, 'standalone_dist'),
-    filename: 'color.min.js',
-    library: 'Color',
-    libraryTarget: 'umd'
-  },
-  plugins: [
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      }
-    }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production')
-    })
-  ],
-  resolve: {
-    extensions: ['', '.js']
-  }
+	entry: path.join(__dirname, 'index'),
+	output: {
+		path: path.join(__dirname, 'standalone_dist'),
+		filename: 'color.min.js',
+		library: 'Color',
+		libraryTarget: 'umd'
+	},
+	plugins: [
+		new webpack.optimize.DedupePlugin(),
+		new webpack.optimize.OccurenceOrderPlugin(),
+		new webpack.optimize.UglifyJsPlugin({
+			compress: {
+				warnings: false
+			}
+		}),
+		new webpack.DefinePlugin({
+			'process.env.NODE_ENV': JSON.stringify('production')
+		})
+	],
+	resolve: {
+		extensions: ['', '.js']
+	}
 };
